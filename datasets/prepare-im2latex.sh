499098b9c60801ce291019dd6316c752241cd308
Chi Ming Chung <marvin852316497@cml12.csie.ntu.edu.tw>
2020-12-09 15:53:25 +0800

    add code to fix path

--- datasets/prepare-im2latex.sh
+++ datasets/prepare-im2latex.sh
@@ -4,6 +4,10 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+fairseq_dir=$(realpath ../../../fairseq)
+spm_train=${fairseq_dir}/scripts/spm_train.py
+spm_encode=${fairseq_dir}/scripts/spm_encode.py
+
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <download_dir> <out_dir>"
   echo "e.g.: $0 /tmp/librispeech_raw/ ~/data/librispeech_final"
@@ -37,8 +41,8 @@ mkdir -p data-tmp/
 # echo "<pad> 1" >> ${dict}
 cp ${download_dir}/im2latex_formulas.norm.lst data-tmp/input.txt
 # spm_train --input=data-tmp/input.txt --vocab_size=${nbpe} --model_type=${bpemode} --model_prefix=${bpemodel} --input_sentence_size=100000000 --unk_id=3 --eos_id=2 --pad_id=1 --bos_id=-1 --character_coverage=1
-spm_train --input=data-tmp/input.txt --vocab_size=${nbpe} --model_type=${bpemode} --model_prefix=${bpemodel} --input_sentence_size=100000000 --character_coverage=1
-spm_encode --model=${bpemodel}.model --output_format=piece < data-tmp/input.txt > ${encoded}
+python $spm_train --input=data-tmp/input.txt --vocab_size=${nbpe} --model_type=${bpemode} --model_prefix=${bpemodel} --input_sentence_size=100000000 --character_coverage=1
+python $spm_encode --model=${bpemodel}.model --output_format=piece < data-tmp/input.txt > ${encoded}
 # echo "Generating dict"
 # cat ${encoded} | tr ' ' '\n' | sort | uniq | awk '{print $0 " " NR+3}' >> ${dict}
 # echo "Generating fairseq-dict"
@@ -46,6 +50,10 @@ spm_encode --model=${bpemodel}.model --output_format=piece < data-tmp/input.txt 
 # cut -f1 ${bpemodel}.vocab | tail -n +4 | sed "s/$/ 100/g" > ${fairseq_dict}
 cut -f1 ${bpemodel}.vocab | tail -n +4 | sed "s/$/ 100/g" > ${fairseq_dict}
 wc -l ${fairseq_dict}
+
+if [ ! -f ${download_dir}/im2latex_valid_filter.lst ]; then
+    ln -s ${download_dir}/im2latex_validate_filter.lst ${download_dir}/im2latex_valid_filter.lst
+fi
 
 for part in train valid test; do
     echo "Prepare $part json"
